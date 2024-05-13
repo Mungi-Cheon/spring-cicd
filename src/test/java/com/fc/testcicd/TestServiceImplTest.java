262fb973bdc2b023286a1cd7d9b1d3d081ecb7fe
Mungi Cheon <c.mungi7421@gmail.com>
2024-05-13 14:29:58 +0900

    push

---
+++ src/test/java/com/fc/testcicd/TestServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.fc.testcicd;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import com.fc.testcicd.service.TestService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+public class TestServiceImplTest {
+
+    @Autowired
+    private TestService testService;
+
+    @Value("${server.port}")
+    private String port;
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Test
+    void checkPortSuccess(){
+        assertEquals(port, "3444");
+    }
+    @Test
+    void checkPortFail(){
+        assertNotEquals(port, "8080");
+    }
+
+    @Test
+    void getTestStringSuccess(){
+        assertEquals(testService.getTestString(), "okay develop server 3444");
+    }
+    @Test
+    void getTestSpringFail(){
+        assertNotEquals(testService.getTestString(), "okay develop server 8080");
+    }
+}
